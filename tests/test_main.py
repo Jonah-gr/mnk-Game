15df149ca8474f9cec4ca96b2dfadfff14b129b9
Jonah Gräfe <jonah.greafe@gmail.com>
2023-11-26 01:50:39 +0100

    bug fixes

--- tests/test_main.py
+++ tests/test_main.py
@@ -2,9 +2,9 @@ from src.main import RandomAI, Minimax, MCTS, Board, Game
 import pytest
 
 @pytest.mark.parametrize("ai1, ai2",
-                        [RandomAI(1), Minimax(2),
-                         Minimax(1), MCTS(2),
-                         MCTS(1), RandomAI(2)])
+                        [(RandomAI(1), Minimax(2)),
+                         (Minimax(1), MCTS(2)),
+                         (MCTS(1), RandomAI(2))])
 def test_main(ai1, ai2):
     game = Game(ai1, ai2)
     game.start()
