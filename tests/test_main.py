a8f89d18c0bbbae31cbff55de4c96f38cb4430b9
Jonah Gräfe <jonah.greafe@gmail.com>
2023-11-26 01:47:20 +0100

    test changes

--- tests/test_main.py
+++ tests/test_main.py
@@ -1,24 +1,10 @@
 from src.main import RandomAI, Minimax, MCTS, Board, Game
+import pytest
 
-def test_main():
-    # Create AI players
-    ai1 = RandomAI(1)
-    ai2 = Minimax(2)
-    ai3 = MCTS(1)
-
-    # Create a game with AI players
+@pytest.mark.parametrize("ai1, ai2",
+                        [RandomAI(1), Minimax(2),
+                         Minimax(1), MCTS(2),
+                         MCTS(1), RandomAI(2)])
+def test_main(ai1, ai2):
     game = Game(ai1, ai2)
-
-    # Start the game
     game.start()
-
-    # Create another game with different AI players
-    game2 = Game(ai2, ai3)
-
-    # Start the second game
-    game2.start()
-
-    # You can continue adding more tests or variations as needed
-
-if __name__ == "__main__":
-    test_main()
